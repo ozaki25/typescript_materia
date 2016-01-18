4bd5f6196feff0b3aacf02c9969606bfbfd924f3
ozaki25 <fs65ugumori@yahoo.co.jp>
2016-01-18 19:31:00 +0900

    新規作成フォームを出せるようにした

--- dist/app/main.js
+++ dist/app/main.js
@@ -13,7 +13,7 @@
     $(function () {
         var stationeryList = [];
         setDefaultStationery();
-        render();
+        renderIndex();
         function setDefaultStationery() {
             stationeryList.push(new Stationery_1.Stationery("鉛筆", 100, 500, "東京"));
             stationeryList.push(new Stationery_1.Stationery("鉛筆", 100, 500, "千葉"));
@@ -25,22 +25,58 @@
             stationeryList.push(new Stationery_1.Stationery("消しゴム", 100, 500, "千葉"));
             stationeryList.push(new Stationery_1.Stationery("消しゴム", 100, 500, "埼玉"));
         }
-        function render() {
+        function renderIndex() {
             $("#contents").empty();
             stationeryList.forEach(function (stationery, index) {
                 console.log(stationery);
                 $("#contents").append(stationery.toHtml(index));
             });
         }
+        function renderNew() {
+            $("#main").html("<form class=\"form-horizontal\">" +
+                "<div class=\"form-group\">" +
+                "<label class=\"col-sm-2 control-label\">商品名</label>" +
+                "<div class=\"col-sm-10\">" +
+                "<input type=\"text\" class=\"form-control\" name=\"brandName\">" +
+                "</div>" +
+                "</div>" +
+                "<div class=\"form-group\">" +
+                "<label class=\"col-sm-2 control-label\">値段</label>" +
+                "<div class=\"col-sm-10\">" +
+                "<input type=\"text\" class=\"form-control\" name=\"price\">" +
+                "</div>" +
+                "</div>" +
+                "<div class=\"form-group\">" +
+                "<label class=\"col-sm-2 control-label\">在庫数</label>" +
+                "<div class=\"col-sm-10\">" +
+                "<input type=\"text\" class=\"form-control\" name=\"quantity\">" +
+                "</div>" +
+                "</div>" +
+                "<div class=\"form-group\">" +
+                "<label class=\"col-sm-2 control-label\">保管場所</label>" +
+                "<div class=\"col-sm-10\">" +
+                "<input type=\"text\" class=\"form-control\" id=\"location\">" +
+                "</div>" +
+                "</div>" +
+                "<div class=\"form-group\">" +
+                "<div class=\"col-sm-offset-2 col-sm-10\">" +
+                "<button id=\"submit\" class=\"btn btn-default\">作成</button>" +
+                "</div>" +
+                "</div>" +
+                "</form>");
+        }
         $(document).on("click", ".receive", function () {
             var id = $(this).data("stationery-id");
             stationeryList[id].receive();
-            render();
+            renderIndex();
         });
         $(document).on("click", ".shipment", function () {
             var id = $(this).data("stationery-id");
             stationeryList[id].shipment();
-            render();
+            renderIndex();
+        });
+        $(document).on("click", "#new_stationery", function () {
+            renderNew();
         });
     });
 });
--- src/app/script/app.ts
+++ src/app/script/app.ts
@@ -4,7 +4,7 @@ import {Stationery} from "./stationery/Stationery";
 $(() => {
     var stationeryList: Stationery[] = [];
     setDefaultStationery();
-    render();
+    renderIndex();
 
     function setDefaultStationery() {
         stationeryList.push(new Stationery("鉛筆", 100, 500, "東京"));
@@ -18,7 +18,7 @@ $(() => {
         stationeryList.push(new Stationery("消しゴム", 100, 500, "埼玉"));
     }
 
-    function render() {
+    function renderIndex() {
         $("#contents").empty();
         stationeryList.forEach((stationery, index) => {
             console.log(stationery);
@@ -26,15 +26,55 @@ $(() => {
         });
     }
 
+    function renderNew() {
+        $("#main").html(
+            "<form class=\"form-horizontal\">" +
+                "<div class=\"form-group\">" +
+                "<label class=\"col-sm-2 control-label\">商品名</label>" +
+                "<div class=\"col-sm-10\">" +
+                "<input type=\"text\" class=\"form-control\" name=\"brandName\">" +
+                "</div>" +
+                "</div>" +
+                "<div class=\"form-group\">" +
+                "<label class=\"col-sm-2 control-label\">値段</label>" +
+                "<div class=\"col-sm-10\">" +
+                "<input type=\"text\" class=\"form-control\" name=\"price\">" +
+                "</div>" +
+                "</div>" +
+                "<div class=\"form-group\">" +
+                "<label class=\"col-sm-2 control-label\">在庫数</label>" +
+                "<div class=\"col-sm-10\">" +
+                "<input type=\"text\" class=\"form-control\" name=\"quantity\">" +
+                "</div>" +
+                "</div>" +
+                "<div class=\"form-group\">" +
+                "<label class=\"col-sm-2 control-label\">保管場所</label>" +
+                "<div class=\"col-sm-10\">" +
+                "<input type=\"text\" class=\"form-control\" id=\"location\">" +
+                "</div>" +
+                "</div>" +
+                "<div class=\"form-group\">" +
+                "<div class=\"col-sm-offset-2 col-sm-10\">" +
+                "<button id=\"submit\" class=\"btn btn-default\">作成</button>" +
+                "</div>" +
+                "</div>" +
+                "</form>"
+        );
+    }
+
     $(document).on("click", ".receive", function() {
         var id: number = $(this).data("stationery-id");
         stationeryList[id].receive();
-        render();
+        renderIndex();
     });
 
     $(document).on("click", ".shipment", function() {
         var id: number = $(this).data("stationery-id");
         stationeryList[id].shipment();
-        render();
+        renderIndex();
+    });
+
+    $(document).on("click", "#new_stationery", function() {
+        renderNew();
     });
 });
